d1c5fb8869567a3866a6faf3fd214cd05d9cc3cf
telltale-san <jonkunapple@gmail.com>
2019-06-28 19:29:02 +0900

    messagesモデルのマイグレートファイルを修正

--- db/migrate/20190620132130_create_messages.rb
+++ db/migrate/20190620132130_create_messages.rb
@@ -2,9 +2,9 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.text :content
-      t.sting :image
-      t.group :references, null: false, foreign_key: true
-      t.user :references, null: false, foreign_key: true
+      t.string :image
+      t.references :group, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
