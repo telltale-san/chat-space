e1fcde5ddb4fd4706443f318b42cc9511a419f8d
telltale-san <jonkunapple@gmail.com>
2019-06-30 08:50:46 +0900

    Update group.rb
    show_last_messageメソッドをgroupクラスに記入

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,14 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    last_message=messages.last
+    if last_message.present?
+      last_message.content? ? last_message.content : "画像が投稿されています"
+    else
+      "まだメッセージはありません"
+    end
+  end
+
 end
