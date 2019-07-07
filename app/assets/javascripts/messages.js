1e57198845ce251567298ad8b9161953e2c02fc3
telltale-san <jonkunapple@gmail.com>
2019-07-08 05:38:22 +0900

    Update messages.js
    ajaxによる非同期通信で、messages#createを呼ぶように編集。
    フォームに入力されたデータの取得にはFormDataを使用。

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -2,7 +2,18 @@ $(function(){
 
   $(".new_message").on("submit",function(e){
     e.preventDefault();
-    console.log("ok!")
+
+    var formData= new FormData(this);
+    var url= $(this).attr("action");
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+
 
   });
 });
