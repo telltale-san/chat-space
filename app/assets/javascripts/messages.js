324bd3d4ac10344d48b626d0eab58a625055f47a
telltale-san <jonkunapple@gmail.com>
2019-07-08 15:34:31 +0900

    Update messages.js
    メッセージまたは画像が投稿された際に、非同期通信によってメッセージビューがつかされる処理を追加

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,5 +1,31 @@
 $(function(){
 
+  var chat__main=$(".chat__main");
+
+
+  function appendMessage(message){
+    var content = message.content ? `${ message.content }` : "";
+    var image = message.image ? `<img src="${ message.image }" width="200" height="200">` : "";
+ 
+    var html =`
+    <div class="chat__main__message">
+      <div class="chat__main__message__name">
+        ${message.user_name}
+      </div>
+      <div class="chat__main__message--gray">
+        ${message.created_at}
+      </div>
+      <div class="chat__main__message__text">
+        ${content}
+        ${image}
+      </div>
+    </div>`;
+
+    chat__main.append(html);
+  };
+
+
+
   $(".new_message").on("submit",function(e){
     e.preventDefault();
 
@@ -13,7 +39,9 @@ $(function(){
       processData: false,
       contentType: false
     })
-
+    .done(function(data){
+      appendMessage(data);
+    })
 
   });
 });
