ac869ae9d46995cb9dcbc2d54c83b92ee292af4c
telltale-san <jonkunapple@gmail.com>
2019-07-09 09:41:10 +0900

    Update users.js
    ・ユーザー検索が成功した際、ユーザーの名前を一覧表示するHTMLが表示される処理を追加。
    ・また、通信のたびに前回の通信で追加されたHTMLを消去する処理も実装。

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,22 @@
 $(function(){
 
+  var searchResult=$("#user-search-result")
+
+  function appendUserHTML(user){
+    var html=`
+    <div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${user.name}</p>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+    </div>  
+    `
+    searchResult.append(html);
+  }
+
+
+
+
+
+
   $(".chat-group-form__input").on("keyup",function(){
     var input=$(this).val();
     
@@ -10,8 +27,11 @@ $(function(){
       dataType: "json"
     })
     .done(function(users){
+      $("#user-search-result").empty();
+      
       users.forEach(function(user){
-        console.log(user);  
+        console.log(user.id);  
+        appendUserHTML(user);
       });
     })
 
