7c375c26a81c148c493ca821709bd6165a992d3e
telltale-san <jonkunapple@gmail.com>
2019-07-09 09:22:40 +0900

    Update users.js
    イベント時に非同期通信できるようにした

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,7 +2,19 @@ $(function(){
 
   $(".chat-group-form__input").on("keyup",function(){
     var input=$(this).val();
-    console.log(input);
+    
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: {keyword: input},
+      dataType: "json"
+    })
+    .done(function(users){
+      users.forEach(function(user){
+        console.log(user);  
+      });
+    })
+
 
   });
 
