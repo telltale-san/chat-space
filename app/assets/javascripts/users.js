3b19158dd761ac4dac9f63c16006b5e7755cb379
telltale-san <jonkunapple@gmail.com>
2019-07-10 18:03:37 +0900

    Update users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,6 @@
 $(function(){
 
+  
   var searchResult=$("#user-search-result")
 
   function appendUserHTML(user){
@@ -11,6 +12,21 @@ $(function(){
     `
     searchResult.append(html);
   }
+
+  var groupMembers=$("#chat-group-users")
+
+  function appendUserToGroup(userName,userId){
+    var html=`
+    <div class='chat-group-user clearfix js-chat-member${userId}' id='chat-group-user-8' data-user-id="${userId}">
+      <input name='group[user_ids][]' type='hidden' value='${userId}'>
+      <p class='chat-group-user__name'>${userName}</p>
+      <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+    </div>
+    `
+    groupMembers.append(html);
+  }
+
+
 
 
   $(".chat-group-form__input").on("keyup",function(){
@@ -24,22 +40,35 @@ $(function(){
     })
     .done(function(users){
       $("#user-search-result").empty();
+      var currentUserId=$(".js-chat-member-current-user").attr("value");
+      console.log(currentUserId);
       users.forEach(function(user){
-        appendUserHTML(user);
-        $("#user-search-result").on("click",`.user-search-add${user.id}`,function(){
-          $(`.chat-group-user.clearfix${user.id}`).remove();
-  
-        });
+        if(user.id==currentUserId){
+          
+        }else{
+          appendUserHTML(user);
+        }
+        
       });
       
-
     })
     .fail(function(){
       alert("ユーザー検索に失敗しました");
     })
   });
   
+  $("#user-search-result").on("click",`.chat-group-user__btn--add`,function(){
 
+    $(this).parent().remove();
+    var userName=$(this).data("user-name");
+    var userId = $(this).data("user-id");
+    appendUserToGroup(userName,userId);
+
+  });
+
+  $("#chat-group-users").on("click",`.chat-group-user__btn--remove`,function(){
+    $(this).parent().remove();
+  });
 
 
 });
